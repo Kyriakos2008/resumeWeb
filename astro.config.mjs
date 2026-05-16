f18552f7601aa9371f75867ffbeff91819f632f6
Kyriakos Nikoletti <78984299+Kyriakos2008@users.noreply.github.com>
2026-05-16 08:29:36 +0300

    Force url

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import icon from 'astro-icon'
 
 export default defineConfig({
     site: 'https://kyriakosnikoletti.com',
-    base: '/resumeWeb/',
+    base: '/',
     integrations: [
         expressiveCode(expressiveCodeOptions),
         tailwind({
